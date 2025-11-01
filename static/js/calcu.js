f3f307437b1f0a5c01eaacef5b8a923e98668f69
Niki96434 <cozercaniegirl143@mail.ru>
2025-11-02 00:57:15 +0300

    calc 50%

--- static/js/calcu.js
+++ static/js/calcu.js
@@ -5,18 +5,29 @@ const buttons = document.querySelectorAll('button');
 const equal = document.getElementById("equal");
 const delItem = document.getElementById("delete");
 
-// del.addEventListener("click", del);
+
 function del() {
     lineInput.innerHTML = "";
 }
+
+function equal() {
+    lineInput.innerHTML = result;
+}
+
 function Calc() {  
         buttons.forEach(btn => {
             btn.addEventListener("click", () => {
-                lineInput.innerHTML += btn.textContent;
-                if (btn.classList.contains("delete")) { // удаление(важно)
-                    delItem.addEventListener("click", del);}
-                }) 
+                const num1 = btn.textContent;
+                const num2 = btn.textContent;
+                lineInput.innerHTML += num1;
+                equal.addEventListener("click", equal)
+                delItem.addEventListener("click", del);})
+                // if (btn.classList.contains("delete")) { // удаление(важно)
+                //     delItem.addEventListener("click", del);}
+                // }) 
         }
     )
 }
+
+
 Calc()
