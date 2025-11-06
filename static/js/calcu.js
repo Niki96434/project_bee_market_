33eaeabd50addc7f982adf9e8908349b02723ae1
Niki96434 <cozercaniegirl143@mail.ru>
2025-11-06 19:31:50 +0300

    calc

--- static/js/calcu.js
+++ static/js/calcu.js
@@ -2,111 +2,156 @@
 
 const parentElement = document.querySelector('.container');
 const lineInput = document.getElementById('input');
-const x = document.querySelectorAll('.button');
-const y = document.querySelectorAll('.button');
+const btnX = document.querySelectorAll('.button');
+// const y = document.querySelectorAll('.button');
 const equal = document.getElementById("equal");
 const delItem = document.getElementById("delete");
 const subtr = document.getElementById("subtr");
 const multi = document.getElementById("multi");
 const deduc = document.getElementById("deduc");
 const adds = document.getElementById("add");
+let arr_X = [];
+let arr_Y = [];
+let xNum = null;
+let yNum = null;
+let second = false;
 let question = null;
-
-function addNumber() {
+function addNumber(xNum, yNum) {
 
     class Calc {
         handleEvent(event) {
             switch(event.target.textContent) {
             case '+': 
+                xNum = parseInt(arr_X.join(''));
+                second = true;
+                //arr_Y = [];
+                console.log(xNum);
                 lineInput.innerHTML += '+';
-                x = null;
                 break
             case 'del':
                 lineInput.innerHTML = '';
-                x = null;
+                xNum = null;
+                yNum = null;
                 break
             case '=':
-                question += parseInt(x);
+                yNum = parseInt(arr_Y.join(''));
+                console.log(yNum);
+                question = xNum + yNum;
+                if (isFinite(question)) {
                 lineInput.innerHTML = question;
+                arr_X = [];
+                arr_Y = [];
+                xNum = null;
+                yNum = null;
+                second = false;}
+                else {lineInput.innerHTML = 'ошибка';}
                 break
             case '1':
-                x = event.target.textContent;
                 lineInput.innerHTML += 1;
-                break
-            }}
-}
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
 
-            function equaly(x) {
-                question = parseInt(x) + parseInt(y);
-                lineInput.innerHTML = question;
-            }
-            
+                break
+            case '2':
+
+                lineInput.innerHTML += 2;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '3':
+                lineInput.innerHTML += 3;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '4':
+                lineInput.innerHTML += 4;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '5':
+
+                lineInput.innerHTML += 5;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '6':
+
+                lineInput.innerHTML += 6;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '7':
+   
+                lineInput.innerHTML += 7;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '8':
+                lineInput.innerHTML += 8;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '9':
+                lineInput.innerHTML += 9;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+            case '0':
+                lineInput.innerHTML += 0;
+                if (second){
+                    arr_Y.push(lineInput.textContent);
+                }
+                else {
+                    arr_X.push(lineInput.textContent);
+                }
+                break
+    }}}
     let calc = new Calc();
     adds.addEventListener("click", calc);
     equal.addEventListener("click", calc);
-    x.forEach(btnX => {
-        btnX.addEventListener("click", calc)});
+    delItem.addEventListener("click", calc);
+    btnX.forEach(btn => {
+            btn.addEventListener("click", calc)});
+    // function equaly() {
+    //                 question = parseInt(xNum) + parseInt(yNum);
+    //                 lineInput.innerHTML = question;
+    //     }
     }
-    y.forEach(btnY => {
-        btnY.addEventListener("click", calc)});
-
 addNumber()
     
-    
-    
-    
-            // if (x === null) {
-            //     btn.addEventListener("click", () => {
-            //         x += btn.textContent;
-            //         lineInput.innerHTML += btn.textContent;
-            //     })}
-            // else {
-            //     btn.addEventListener("click", () => {
-            //         y += btn.textContent;
-            //         lineInput.innerHTML += btn.textContent;
-            //     })
-            // }}
-
-//     action(action) {
-//         switch (action) {
-//         case adds:
-//             this.question = this.x + this.y;
-//             break
-//         case equal:
-//             return this.question
-//             break
-//         case del:
-//             return this.question = null;
-//     }
-// }
-
-// function del() {
-//     lineInput.innerHTML = "";
-// }
-
-// function question(numberX,numberY) {
-//     numberQuestion = numberX + numberY;
-//     lineInput.innerHTML = numberQuestion;
-// }
-
-// function Calc() {  
-//         buttons.forEach(btn => {
-//         btn.addEventListener("click", function inputItem() {
-//                     btnTextX = btn.textContent; // 1е число
-//                     numberX = parseInt(btnTextX); // 1е число инт
-//                     lineInput.innerHTML += btnTextX; // выводим
-//                     btn.removeEventListener("click", inputItem)}) // удаляем эту функцию
-//         adds.addEventListener("click", () => {
-//                     lineInput.innerHTML = btnTextX + '+';
-//                     btn.addEventListener("click", () => {
-//                         btnTextY = btn.textContent; // 2е число
-//                         numberY = parseInt(btnTextY); 
-//                         lineInput.innerHTML += btnTextY;})
-//                     })
-//                 })
-
-//         delItem.addEventListener("click", del);
-//         equal.addEventListener("click", question);
-//             }
-
-// Calc()
